87fa139cba8b7d53df3ae617672bb995e10bfb7e
Damien Gaiga <gaigada@gaigada-mac.local>
2021-02-12 16:53:41 +0100

    Move settings in a component

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,22 +1,12 @@
 import * as React from 'react';
 import { useState } from 'react';
-// NOTE: React Native Paper modal doesn't work fine - DON'T USE
-// NOTE: React Native Paper ToggleButton can't allow add text in the button -> Custom Toggle Button here
-import { View, FlatList, StyleSheet, Modal } from 'react-native';
-import {
-  Dialog,
-  Button,
-  Appbar,
-  Searchbar,
-  Surface,
-  IconButton,
-  Text,
-  RadioButton,
-} from 'react-native-paper';
+import { View, FlatList, StyleSheet } from 'react-native';
+import { Appbar, Searchbar, Surface, IconButton } from 'react-native-paper';
 import Item from './Item';
 import logger from '../Core/LoggerSingleton';
 // @ts-ignore
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Settings } from '../Components/Settings';
 
 interface IProps {
   testId?: string;
@@ -43,10 +33,15 @@ export const Main = (props: IProps) => {
     .reverse();
   logger.setCallback(setRequests);
 
+  const clearList = () => {
+    logger.clear();
+    setRequests(logger.getRequests());
+  };
+
   return (
     <>
       <Appbar.Header>
-        <IconButton icon='close' onPress={() => props.onPressClose(false)} />
+        <IconButton icon="close" onPress={() => props.onPressClose(false)} />
         <Appbar.Content title="Netwatch" />
       </Appbar.Header>
       <Surface style={styles.surface}>
@@ -64,84 +59,15 @@ export const Main = (props: IProps) => {
         ></IconButton>
       </Surface>
 
-      <Modal animationType="fade" transparent visible={settingsVisible}>
-        <Dialog visible={settingsVisible} onDismiss={hideDialog}>
-          <Dialog.Content>
-            <Dialog.Title>Settings</Dialog.Title>
-            <RadioButton.Group onValueChange={(value) => setSource(value)} value={source}>
-              <View style={styles.radioButtonContainer}>
-                <RadioButton value="rnr" />
-                <Text style={styles.radioButtonText}>React Native Requests</Text>
-              </View>
-              <View style={styles.radioButtonContainer}>
-                <RadioButton value="nr" />
-                <Text style={styles.radioButtonText}>Native Requests</Text>
-              </View>
-            </RadioButton.Group>
-
-            <Dialog.Title>Filter</Dialog.Title>
-            <View style={styles.toggleButtonGroup}>
-              <Button
-                labelStyle={[
-                  styles.toggleButtonLabel,
-                  filter === 'all' && styles.toogleButtonStatus,
-                ]}
-                style={[styles.toggleButton, filter === 'all' && styles.toggleButtonOpacity]}
-                onPress={() => setFilter('all')}
-              >
-                All
-              </Button>
-              <Button
-                labelStyle={[
-                  styles.toggleButtonLabel,
-                  filter === 'get' && styles.toogleButtonStatus,
-                ]}
-                style={[styles.toggleButton, filter === 'get' && styles.toggleButtonOpacity]}
-                onPress={() => setFilter('get')}
-              >
-                GET
-              </Button>
-              <Button
-                labelStyle={[
-                  styles.toggleButtonLabel,
-                  filter === 'post' && styles.toogleButtonStatus,
-                ]}
-                style={[styles.toggleButton, filter === 'post' && styles.toggleButtonOpacity]}
-                onPress={() => setFilter('post')}
-              >
-                POST
-              </Button>
-              <Button
-                labelStyle={[
-                  styles.toggleButtonLabel,
-                  filter === 'put' && styles.toogleButtonStatus,
-                ]}
-                style={[styles.toggleButton, filter === 'put' && styles.toggleButtonOpacity]}
-                onPress={() => setFilter('put')}
-              >
-                PUT
-              </Button>
-              <Button
-                labelStyle={[
-                  styles.toggleButtonLabel,
-                  filter === 'delete' && styles.toogleButtonStatus,
-                ]}
-                style={[
-                  styles.toggleButton,
-                  filter === 'delete' && styles.toggleButtonOpacity,
-                  { borderRightWidth: 0 },
-                ]}
-                onPress={() => setFilter('delete')}
-              >
-                DEL
-              </Button>
-            </View>
-          </Dialog.Content>
-          <Dialog.Actions>
-            <Button onPress={hideDialog}>Done</Button>
-          </Dialog.Actions>
-        </Dialog>
-      </Modal>
+      <Settings
+        visible={settingsVisible}
+        onDismiss={hideDialog}
+        source={source}
+        onSetSource={setSource}
+        filter={filter}
+        onSetFilter={setFilter}
+        onPressClear={clearList}
+      />
 
       <View style={styles.centeredView}>
         <View style={styles.modalView}>
@@ -166,9 +92,9 @@ export const Main = (props: IProps) => {
 };
 
 const styles = StyleSheet.create({
-  searchBar: { 
-    flex: 1, 
-    backgroundColor: 'gray' 
+  searchBar: {
+    flex: 1,
+    backgroundColor: 'gray',
   },
   surface: {
     padding: 16,
@@ -189,39 +115,5 @@ const styles = StyleSheet.create({
   modalView: {
     width: '100%',
     height: '100%',
-  },
-  toggleButtonGroup: {
-    minHeight: 38,
-    width: '100%',
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    borderWidth: 0.5,
-    borderColor: 'white',
-    borderRadius: 5,
-  },
-  toggleButton: {
-    borderRightWidth: 0.5,
-    borderColor: 'white',
-  },
-  toggleButtonLabel: {
-    color: 'white',
-  },
-  toogleButtonStatus: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  toggleButtonOpacity: {
-    opacity: 0.5,
-  },
-  radioButtonContainer: {
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  radioButtonText: {
-    textAlignVertical: 'center',
-    fontWeight: 'bold',
   },
 });
