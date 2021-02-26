64f3582db70f14b2ee7ccdca4eb1b68b9ac97372
Damien Gaiga <gaigada@gaigada-mac.local>
2021-02-26 18:51:46 +0100

    Fix scroll issue Settings

--- src/Components/Settings.tsx
+++ src/Components/Settings.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useContext } from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import { RadioButton } from 'react-native-paper';
 import { ThemeContext } from '../Theme';
 import { SourceType, RequestMethod, EnumFilterType, EnumSourceType } from '../types';
@@ -27,137 +27,142 @@ export const Settings = (props: IProps) => {
 
   return (
     <View style={[styles.container, { backgroundColor: theme.gray900 }]}>
-      <View style={{ flex: 1 }}>
-        <Title style={[styles.title, { color: theme.gray50 }]}>Display:</Title>
-        <RadioButton.Group onValueChange={(value) => props.onSetSource(value)} value={props.source}>
-          <RadioButton.Item
-            value={EnumSourceType.All}
-            status={checkedSource === EnumSourceType.All ? 'checked' : 'unchecked'}
-            onPress={() => setCheckedSource(EnumSourceType.All)}
-            testID={'RadioAllSource'}
-            label="All Requests and Actions"
-            mode="android"
-            style={styles.radioButton}
-            labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
-            color={theme.blue500}
-            uncheckedColor={theme.blue500}
-          />
+      <ScrollView style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <Title style={[styles.title, { color: theme.gray50 }]}>Display:</Title>
+          <RadioButton.Group
+            onValueChange={(value) => props.onSetSource(value)}
+            value={props.source}
+          >
+            <RadioButton.Item
+              value={EnumSourceType.All}
+              status={checkedSource === EnumSourceType.All ? 'checked' : 'unchecked'}
+              onPress={() => setCheckedSource(EnumSourceType.All)}
+              testID={'RadioAllSource'}
+              label="All Requests and Actions"
+              mode="android"
+              style={styles.radioButton}
+              labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
+              color={theme.blue500}
+              uncheckedColor={theme.blue500}
+            />
 
-          <RadioButton.Item
-            value={EnumSourceType.Redux}
-            status={checkedSource === EnumSourceType.Redux ? 'checked' : 'unchecked'}
-            onPress={() => setCheckedSource(EnumSourceType.Redux)}
-            testID={'RadioRedux'}
-            label="Redux Action"
-            mode="android"
-            style={styles.radioButton}
-            labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
-            color={theme.blue500}
-            uncheckedColor={theme.blue500}
-          />
+            <RadioButton.Item
+              value={EnumSourceType.Redux}
+              status={checkedSource === EnumSourceType.Redux ? 'checked' : 'unchecked'}
+              onPress={() => setCheckedSource(EnumSourceType.Redux)}
+              testID={'RadioRedux'}
+              label="Redux Action"
+              mode="android"
+              style={styles.radioButton}
+              labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
+              color={theme.blue500}
+              uncheckedColor={theme.blue500}
+            />
 
-          <RadioButton.Item
-            value={EnumSourceType.ReactNativeRequest}
-            status={checkedSource === EnumSourceType.ReactNativeRequest ? 'checked' : 'unchecked'}
-            onPress={() => setCheckedSource(EnumSourceType.ReactNativeRequest)}
-            testID={'RadioRNR'}
-            label="React Native Requests"
-            labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
-            mode="android"
-            style={styles.radioButton}
-            color={theme.blue500}
-            uncheckedColor={theme.blue500}
-          />
+            <RadioButton.Item
+              value={EnumSourceType.ReactNativeRequest}
+              status={checkedSource === EnumSourceType.ReactNativeRequest ? 'checked' : 'unchecked'}
+              onPress={() => setCheckedSource(EnumSourceType.ReactNativeRequest)}
+              testID={'RadioRNR'}
+              label="React Native Requests"
+              labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
+              mode="android"
+              style={styles.radioButton}
+              color={theme.blue500}
+              uncheckedColor={theme.blue500}
+            />
 
-          <RadioButton.Item
-            value={EnumSourceType.Nativerequest}
-            status={checkedSource === EnumSourceType.Nativerequest ? 'checked' : 'unchecked'}
-            onPress={() => setCheckedSource(EnumSourceType.Nativerequest)}
-            testID={'RadioNR'}
-            label="Native Requests"
-            labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
-            mode="android"
-            style={styles.radioButton}
-            color={theme.blue500}
-            uncheckedColor={theme.blue500}
-          />
-        </RadioButton.Group>
+            <RadioButton.Item
+              value={EnumSourceType.Nativerequest}
+              status={checkedSource === EnumSourceType.Nativerequest ? 'checked' : 'unchecked'}
+              onPress={() => setCheckedSource(EnumSourceType.Nativerequest)}
+              testID={'RadioNR'}
+              label="Native Requests"
+              labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
+              mode="android"
+              style={styles.radioButton}
+              color={theme.blue500}
+              uncheckedColor={theme.blue500}
+            />
+          </RadioButton.Group>
 
-        {!disabledFilter && (
-          <>
-            <Title style={[styles.title, { color: theme.gray50 }]}>Type:</Title>
-            <RadioButton.Group
-              onValueChange={(value) => props.onSetFilter(value as EnumFilterType)}
-              value={props.filter}
-            >
-              <RadioButton.Item
-                value={EnumFilterType.All}
-                status={checkedFilter === EnumFilterType.All ? 'checked' : 'unchecked'}
-                onPress={() => setCheckedFilter(EnumFilterType.All)}
-                testID={'RadioAllMethod'}
-                label="ALL"
-                mode="android"
-                style={styles.radioButton}
-                labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
-                color={theme.blue500}
-                uncheckedColor={theme.blue500}
-              />
+          {!disabledFilter && (
+            <>
+              <Title style={[styles.title, { color: theme.gray50 }]}>Type:</Title>
+              <RadioButton.Group
+                onValueChange={(value) => props.onSetFilter(value as EnumFilterType)}
+                value={props.filter}
+              >
+                <RadioButton.Item
+                  value={EnumFilterType.All}
+                  status={checkedFilter === EnumFilterType.All ? 'checked' : 'unchecked'}
+                  onPress={() => setCheckedFilter(EnumFilterType.All)}
+                  testID={'RadioAllMethod'}
+                  label="ALL"
+                  mode="android"
+                  style={styles.radioButton}
+                  labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
+                  color={theme.blue500}
+                  uncheckedColor={theme.blue500}
+                />
 
-              <RadioButton.Item
-                value={EnumFilterType.Get}
-                status={checkedFilter === EnumFilterType.Get ? 'checked' : 'unchecked'}
-                onPress={() => setCheckedFilter(EnumFilterType.Get)}
-                testID={'RadioGet'}
-                label="GET"
-                mode="android"
-                style={styles.radioButton}
-                labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
-                color={theme.blue500}
-                uncheckedColor={theme.blue500}
-              />
+                <RadioButton.Item
+                  value={EnumFilterType.Get}
+                  status={checkedFilter === EnumFilterType.Get ? 'checked' : 'unchecked'}
+                  onPress={() => setCheckedFilter(EnumFilterType.Get)}
+                  testID={'RadioGet'}
+                  label="GET"
+                  mode="android"
+                  style={styles.radioButton}
+                  labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
+                  color={theme.blue500}
+                  uncheckedColor={theme.blue500}
+                />
 
-              <RadioButton.Item
-                value={EnumFilterType.Post}
-                status={checkedFilter === EnumFilterType.Post ? 'checked' : 'unchecked'}
-                onPress={() => setCheckedFilter(EnumFilterType.Post)}
-                testID={'RadioPost'}
-                label="POST"
-                labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
-                mode="android"
-                style={styles.radioButton}
-                color={theme.blue500}
-                uncheckedColor={theme.blue500}
-              />
+                <RadioButton.Item
+                  value={EnumFilterType.Post}
+                  status={checkedFilter === EnumFilterType.Post ? 'checked' : 'unchecked'}
+                  onPress={() => setCheckedFilter(EnumFilterType.Post)}
+                  testID={'RadioPost'}
+                  label="POST"
+                  labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
+                  mode="android"
+                  style={styles.radioButton}
+                  color={theme.blue500}
+                  uncheckedColor={theme.blue500}
+                />
 
-              <RadioButton.Item
-                value={EnumFilterType.Put}
-                status={checkedFilter === EnumFilterType.Put ? 'checked' : 'unchecked'}
-                onPress={() => setCheckedFilter(EnumFilterType.Put)}
-                testID={'RadioPut'}
-                label="PUT"
-                labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
-                mode="android"
-                style={styles.radioButton}
-                color={theme.blue500}
-                uncheckedColor={theme.blue500}
-              />
+                <RadioButton.Item
+                  value={EnumFilterType.Put}
+                  status={checkedFilter === EnumFilterType.Put ? 'checked' : 'unchecked'}
+                  onPress={() => setCheckedFilter(EnumFilterType.Put)}
+                  testID={'RadioPut'}
+                  label="PUT"
+                  labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
+                  mode="android"
+                  style={styles.radioButton}
+                  color={theme.blue500}
+                  uncheckedColor={theme.blue500}
+                />
 
-              <RadioButton.Item
-                value={EnumFilterType.Delete}
-                status={checkedFilter === EnumFilterType.Delete ? 'checked' : 'unchecked'}
-                onPress={() => setCheckedFilter(EnumFilterType.Delete)}
-                testID={'RadioDelete'}
-                label="DELETE"
-                labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
-                mode="android"
-                style={styles.radioButton}
-                color={theme.blue500}
-                uncheckedColor={theme.blue500}
-              />
-            </RadioButton.Group>
-          </>
-        )}
-      </View>
+                <RadioButton.Item
+                  value={EnumFilterType.Delete}
+                  status={checkedFilter === EnumFilterType.Delete ? 'checked' : 'unchecked'}
+                  onPress={() => setCheckedFilter(EnumFilterType.Delete)}
+                  testID={'RadioDelete'}
+                  label="DELETE"
+                  labelStyle={[styles.radioButtonLabel, { color: theme.gray50 }]}
+                  mode="android"
+                  style={styles.radioButton}
+                  color={theme.blue500}
+                  uncheckedColor={theme.blue500}
+                />
+              </RadioButton.Group>
+            </>
+          )}
+        </View>
+      </ScrollView>
       <View>
         <TouchableOpacity
           testID={'ResetButton'}
