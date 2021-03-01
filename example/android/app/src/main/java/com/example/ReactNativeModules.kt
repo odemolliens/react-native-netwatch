3440e2743e3e4efdd2d53d5958f642e99c3cf700
Damien Gaiga <gaigada@gaigada-mac.local>
2021-03-01 13:37:03 +0100

    Setup react-native-fs

--- example/android/app/src/main/java/com/example/ReactNativeModules.kt
+++ example/android/app/src/main/java/com/example/ReactNativeModules.kt
@@ -1,6 +1,7 @@
 package com.example
 
 import android.view.View
+import com.rnfs.RNFSPackage
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
